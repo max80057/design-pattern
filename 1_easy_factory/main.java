5764331dec4b941967a57d8f6f2ceacc456fce2f
max80057 <max80057@gmail.com>
2021-10-06 15:04:17 +0800

    Add files via upload

---
+++ 1_easy_factory/main.java
@@ -0,0 +1,25 @@
+public class main {
+
+    public static void main(String[] args) {
+        Operation work = new EasyFactory().create("add");
+        work.setNumberA(63);
+        work.setNumberB(7);
+        System.out.println(work.getResult());
+
+        work = new EasyFactory().create("sub");
+        work.setNumberA(63);
+        work.setNumberB(7);
+        System.out.println(work.getResult());
+
+        work = new EasyFactory().create("mul");
+        work.setNumberA(63);
+        work.setNumberB(7);
+        System.out.println(work.getResult());
+
+        work = new EasyFactory().create("div");
+        work.setNumberA(63);
+        work.setNumberB(7);
+        System.out.println(work.getResult());
+
+    }
+}
